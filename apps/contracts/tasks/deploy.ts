bb3b071907b3b63b5115ce62226123cc7abeced2
ByeongHunKim <rkwhr912@naver.com>
2023-04-14 17:48:37 +0900

    🎉 new feedback contract

--- apps/contracts/tasks/deploy.ts
+++ apps/contracts/tasks/deploy.ts
@@ -3,8 +3,9 @@ import { task, types } from "hardhat/config"
 task("deploy", "Deploy a Feedback contract")
     .addOptionalParam("semaphore", "Semaphore contract address", undefined, types.string)
     .addOptionalParam("group", "Group id", "42", types.string)
+    .addOptionalParam("nft", "NFT contract address", undefined, types.string)
     .addOptionalParam("logs", "Print the logs", true, types.boolean)
-    .setAction(async ({ logs, semaphore: semaphoreAddress, group: groupId }, { ethers, run }) => {
+    .setAction(async ({ logs, semaphore: semaphoreAddress, group: groupId, nft: nftAddress }, { ethers, run }) => {
         if (!semaphoreAddress) {
             const { semaphore } = await run("deploy:semaphore", {
                 logs
@@ -17,14 +18,18 @@ task("deploy", "Deploy a Feedback contract")
             groupId = process.env.GROUP_ID
         }
 
+        if (!nftAddress) { // default value = 0xF268C79d48E168d09e2e3A406b5E85337C39d226 ( 예전에 사용했던 nft contract )
+            nftAddress = process.env.NFT_CONTRACT_ADDRESS
+        }
+
         const FeedbackFactory = await ethers.getContractFactory("Feedback")
 
-        const feedbackContract = await FeedbackFactory.deploy(semaphoreAddress, groupId)
+        const feedbackContract = await FeedbackFactory.deploy(semaphoreAddress, groupId, nftAddress)
 
         await feedbackContract.deployed()
 
         if (logs) {
-            console.info(`Feedback contract has been deployed to: ${feedbackContract.address}`)
+            console.info(`Feedback contract has been deployed to: ${feedbackContract.address} with NFT contract : ${nftAddress}` )
         }
 
         return feedbackContract
--- apps/contracts/test/Feedback.ts
+++ apps/contracts/test/Feedback.ts
@@ -14,6 +14,7 @@ describe("Feedback", () => {
 
     const groupId = "42"
     const group = new Group(groupId)
+    const nftAddress = process.env.NFT_CONTRACT_ADDRESS
     const users: Identity[] = []
 
     before(async () => {
@@ -21,7 +22,9 @@ describe("Feedback", () => {
             logs: false
         })
 
-        feedbackContract = await run("deploy", { logs: false, group: groupId, semaphore: semaphore.address })
+        feedbackContract = await run("deploy", { logs: false, group: groupId, semaphore: semaphore.address, nft: nftAddress })
+        console.log(`      [test] feedbackContract is deployed successfully with ${nftAddress}`)
+
         semaphoreContract = semaphore
 
         users.push(new Identity())
@@ -30,8 +33,10 @@ describe("Feedback", () => {
 
     describe("# joinGroup", () => {
         it("Should allow users to join the group", async () => {
-            for await (const [i, user] of users.entries()) {
-                const transaction = feedbackContract.joinGroup(user.commitment)
+            console.log('check users array >> ', users.slice((1))) // 0으로 하면 첫 번째 요소까지 루프에서 반복되기 때문에 이전에 가입한 첫 번째 유저가 다시 추가되어 에러 발생
+            for await (const [i, user] of users.slice(1).entries()) { // 이전에 가입한 첫 번째 유저를 제외하고 새로운 유저 추가
+                const transaction = feedbackContract.joinGroup(user.commitment, formatBytes32String("nickname1"))
+                console.log('# joinGroup - user.commitment is ', user.commitment)
 
                 group.addMember(user.commitment)
 
@@ -41,6 +46,7 @@ describe("Feedback", () => {
             }
         })
     })
+
 
     describe("# sendFeedback", () => {
         const wasmFilePath = `${config.paths.build["snark-artifacts"]}/semaphore.wasm`
@@ -66,4 +72,11 @@ describe("Feedback", () => {
                 .withArgs(groupId, fullProof.merkleTreeRoot, fullProof.nullifierHash, groupId, fullProof.signal)
         })
     })
+
+    describe("# getUserInfo", () => {
+        it("Should allow user to get each of identityCommitment ", async () => {
+            const result = await feedbackContract.getUserInfo(formatBytes32String("nickname1"));
+            console.log('# getUserInfo - identityCommitment of user is ', result.toString());
+        });
+    });
 })
